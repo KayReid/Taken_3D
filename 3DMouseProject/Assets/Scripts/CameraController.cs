bfc278bd29d1e046a94d925e6655aa5bc1b17985
zleesh <arz7cn@virginia.edu>
2018-04-11 14:06:30 +0200

    Smooth camera controlling

--- 3DMouseProject/Assets/Scripts/CameraController.cs
+++ 3DMouseProject/Assets/Scripts/CameraController.cs
@@ -16,6 +16,21 @@ public class CameraController : MonoBehaviour {
 
     // Update is called once per frame
     void LateUpdate() {
-        transform.position = target.transform.position + offset;
+        //transform.position = target.transform.position + offset;
+
+
+        Vector3 newPos = transform.position;
+        Vector3 targetPosition = target.position + offset;
+
+        // Creates a smoother animation - the farther the target from its original place, the faster the camera moves to catch it.
+        if ((newPos - targetPosition).magnitude > minSpeed * Time.deltaTime)
+        {
+            Vector3 targetDir = targetPosition - newPos;
+            targetDir.Normalize();
+            newPos += targetDir * (Time.deltaTime * minSpeed);
+        }
+
+        transform.position = newPos;
+
     }
 }
