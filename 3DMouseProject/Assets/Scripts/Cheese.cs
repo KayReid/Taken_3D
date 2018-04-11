ea427a162594f680d55874fb3ebe94672e4a407b
Binh Vo <binhvo07@gmail.com>
2018-04-11 13:04:05 +0200

    Added Interface
    Added Collectible interface for items

--- 3DMouseProject/Assets/Scripts/Cheese.cs
+++ 3DMouseProject/Assets/Scripts/Cheese.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cheese : MonoBehaviour {
+public class Cheese : MonoBehaviour, Collectibles {
     int cheeseCounter; 
 
 	// Use this for initialization
