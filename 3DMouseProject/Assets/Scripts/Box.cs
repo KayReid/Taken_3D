5916ec8313cef38a36a7ab410e191c46277ab258
zleesh <arz7cn@virginia.edu>
2018-04-12 11:18:04 +0200

    Added destroyable box
    box can have object inside or nothing

---
+++ 3DMouseProject/Assets/Scripts/Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    [Tooltip("An optional item that will appear when the box is destroyed.")]
+    [SerializeField] GameObject item = null;
+
+    // Destroy box when shot 
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Poop")
+        {
+            if (item != null)
+            {
+                Instantiate(item);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
