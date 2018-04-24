2b97596bb076446010f48792247fdaf6983315c6
Kreator <Kayl@Kayleighs-Air.DisStu.local>
2018-04-24 17:42:47 +0200

    New AI script

---
+++ 3DMouseProject/Assets/Scripts/NPC.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour {
+
+	[SerializeField]
+	Transform _destination;
+
+	NavMeshAgent _navMeshagent;
+
+	// Use this for initialization
+	public void Start (){
+		_navMeshagent = this.GetComponent<NavMeshAgent>();
+
+		if(_navMeshagent == null){
+			Debug.LogError("Nav Mesh Agent component not found attached to " + gameObject.name);
+		}
+		else{
+			SetDestination();
+		}
+	}
+
+	void Update () {
+		SetDestination ();
+	}
+
+	private void SetDestination(){
+		if(_destination != null){
+			Vector3 targetVector = _destination.transform.position;
+
+			if (Vector3.Distance (_destination.transform.position, _navMeshagent.transform.position) <= 1) {
+				_navMeshagent.Stop();
+			} else {
+				_navMeshagent.Resume();
+			}
+
+			_navMeshagent.SetDestination(targetVector);
+		}
+	}
+}
