76ecb0dfb246b851c92fe874d8c5028fe14b794f
Kreator <Kayl@Kayleighs-Air.DisStu.local>
2018-04-24 18:19:37 +0200

    Change plz

--- 3DMouseProject/Assets/Scripts/NPC.cs
+++ 3DMouseProject/Assets/Scripts/NPC.cs
@@ -30,11 +30,15 @@ public class NPC : MonoBehaviour {
 		if(_destination != null){
 			Vector3 targetVector = _destination.transform.position;
 
-			if (Vector3.Distance (_destination.transform.position, _navMeshagent.transform.position) <= 2 || 
-				Vector3.Distance (_destination.transform.position, _navMeshagent.transform.position) >= 20) {
+			if (Vector3.Distance (_destination.transform.position, _navMeshagent.transform.position) <= 2){
 				_navMeshagent.Stop();
 			} else {
 				_navMeshagent.Resume();
+			}
+
+			if (Vector3.Distance (_destination.transform.position, _navMeshagent.transform.position) >= 20) {
+				// MAKE THE ENEMY WONDER AWAY
+				_navMeshagent.Stop;
 			}
 
 			_navMeshagent.SetDestination(targetVector);
