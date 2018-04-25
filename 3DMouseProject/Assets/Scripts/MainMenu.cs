dbf5ccd1bf7ac82fe9d17adc8a30554c9f83bc9f
Binh Vo <binhvo07@gmail.com>
2018-04-25 18:00:26 +0200

    Added main menu

---
+++ 3DMouseProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public string firstLevel;
+
+    public GameObject mainMenu;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 
+		
+	}
+
+    public void NewGame() {
+        SceneManager.LoadScene(firstLevel);    
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+
+}
