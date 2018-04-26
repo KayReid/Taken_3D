e82c61208fc401f5b0844d8ddd6f5d317cd96edb
zleesh <arz7cn@virginia.edu>
2018-04-26 12:46:01 +0200

    Finished UI

--- 3DMouseProject/Assets/Scripts/Collectible/Canon.cs
+++ 3DMouseProject/Assets/Scripts/Collectible/Canon.cs
@@ -7,9 +7,9 @@ public class Canon : MonoBehaviour{
     /// <summary>
     /// If touched by player, will be destroyed by impact and "added" to UI. Used to win the game.
     /// </summary>
-    /// <param name="other"></param>
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
+    /// <param name="collision"></param>
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Player") {
             UICollectible.instance.UpdateInventory(this.gameObject); // Update UI
             Destroy(this.gameObject);
         }
