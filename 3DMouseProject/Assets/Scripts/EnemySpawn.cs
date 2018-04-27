432f73928bf834ce8000ba1817fb585390bf4d86
celia96 <celiachoy96@gmail.com>
2018-04-27 18:00:17 +0200

    EnemySpawn

---
+++ 3DMouseProject/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+
+	public GameObject catPrefab;
+	public GameObject dogPrefab;
+	List<GameObject> enemyList = new List<GameObject>();
+	int prefabIndex;
+
+
+	// Use this for initialization
+	void Start () {
+		enemyList.Add(catPrefab);
+		enemyList.Add(dogPrefab);
+		StartCoroutine (SpawnEnemyCoroutine ());
+	}
+
+
+	IEnumerator SpawnEnemyCoroutine () {
+		// forever
+		while (true) {
+
+			prefabIndex = UnityEngine.Random.Range(0,1);
+			Vector3 spawnPosition = transform.position;
+			Instantiate(enemyList[prefabIndex], spawnPosition, Quaternion.identity);
+
+			// yield return new WaitForSeconds(Random.Range(10,15));
+			yield return new WaitForSeconds(2);
+
+		}
+
+	}
+}
