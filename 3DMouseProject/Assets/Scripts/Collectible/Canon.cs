f13fba501743d07672d38daa788c1fa306deca8f
zleesh <arz7cn@virginia.edu>
2018-04-27 21:58:54 +0200

    Update collectible sounds

--- 3DMouseProject/Assets/Scripts/Collectible/Canon.cs
+++ 3DMouseProject/Assets/Scripts/Collectible/Canon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Canon : MonoBehaviour{
 
 	public static int canonCounter;
+    public AudioClip collectSound;
     /// <summary>
     /// If touched by player, will be destroyed by impact and "added" to UI. Used to win the game.
     /// </summary>
@@ -12,6 +13,7 @@ public class Canon : MonoBehaviour{
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player") {
             UICollectible.instance.UpdateInventory(this.gameObject); // Update UI
+            AudioSource.PlayClipAtPoint(collectSound, transform.position); // play sound clip
             Destroy(this.gameObject);
         }
     }
--- 3DMouseProject/Assets/Scripts/Collectible/Key.cs
+++ 3DMouseProject/Assets/Scripts/Collectible/Key.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Key : MonoBehaviour {
 
     public static bool keyActivated;
+    public AudioClip collectSound;
 
     /// <summary>
     /// If touched by player, will be destroyed on impact and "added" to UI. Used to free wife from her cage.
@@ -13,6 +14,7 @@ public class Key : MonoBehaviour {
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player") {
             UICollectible.instance.UpdateInventory(this.gameObject); // Update UI
+            AudioSource.PlayClipAtPoint(collectSound, transform.position); // play sound clip
             Destroy(this.gameObject); // Destroy this key object
             keyActivated = true;
             Debug.Log("Player hit!, Key is activated for the door");
